d01672c62563c1c7cd50bf1c42665efc4f4c14ed
sonbyo-ship-it <sonbyo@gmail.com>
2026-06-23 17:44:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,83 @@
-function analyze(){
+const API_KEY = "F63D7LA66HK0709S";
 
-const ticker =
-document.getElementById("ticker").value;
+async function analyze() {
 
-document.getElementById("result").innerHTML =
-`
-<h3>${ticker}</h3>
-<p>분석 기능 준비중</p>
+const symbol =
+document
+.getElementById("ticker")
+.value
+.toUpperCase()
+.trim();
+
+const result =
+document.getElementById("result");
+
+if(!symbol){
+result.innerHTML =
+"<p>종목코드를 입력하세요.</p>";
+return;
+}
+
+result.innerHTML =
+'<p class="loading">데이터 조회중...</p>';
+
+try{
+
+const response =
+await fetch(
+`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${API_KEY}`
+);
+
+const data =
+await response.json();
+
+if(!data.Symbol){
+
+result.innerHTML =
+"<p>종목을 찾을 수 없습니다.</p>";
+
+return;
+}
+
+result.innerHTML = `
+<div class="card">
+
+<h2>${data.Name}</h2>
+
+<p><strong>종목코드</strong> :
+${data.Symbol}</p>
+
+<p><strong>업종</strong> :
+${data.Industry}</p>
+
+<p><strong>PER</strong> :
+${data.PERatio}</p>
+
+<p><strong>PBR</strong> :
+${data.PriceToBookRatio}</p>
+
+<p><strong>시가총액</strong> :
+${Number(
+data.MarketCapitalization
+).toLocaleString()}</p>
+
+<p><strong>52주 최고가</strong> :
+${data["52WeekHigh"]}</p>
+
+<p><strong>52주 최저가</strong> :
+${data["52WeekLow"]}</p>
+
+</div>
 `;
+
+}
+catch(error){
+
+result.innerHTML =
+"<p>데이터 조회 실패</p>";
+
+console.error(error);
+
+}
 
 }
